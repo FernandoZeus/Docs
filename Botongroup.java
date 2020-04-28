8c526fe6aa6bcda04c9bb93f44f9507f847fa843
Fernando <62989915+FernandoZeus@users.noreply.github.com>
2020-04-28 11:26:42 +0200

    Add files via upload

--- Botongroup.java
+++ Botongroup.java
@@ -1,46 +1,79 @@
-import java.util.Date;
-import java.util.ArrayList;
-import javax.swing.ButtonGroup;
-import javax.swing.JRadioButton;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.FlowLayout;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
+import java.awt.event.*;
 
-public class Botones{
-    public static void main(String[] args) {
-      
-       JRadioButton boton1=new JRadioButton("NÚMERO 1");
-       JRadioButton boton2=new JRadioButton("NÚMERO 2");
+public class Fzo0424 extends JFrame{
+
+JRadioButton jRadioButton1;  
+    JRadioButton jRadioButton2;
+    JButton jButton;
+    JButton jButton2;
+    ButtonGroup G1; //agrupación de radio button
+    JLabel L1;
+    JLabel L2;
+    Border loweredbevel;
+   
+    public Fzo0424() {
+ 
+        this.setLayout(null);    
+        setVisible(true);
+        this.setBounds(100, 100, 400, 200);
+        //color del framethis.setBackground(Color.CYAN);
      
-        ButtonGroup group = new ButtonGroup();
-        group.add(boton1);
-        group.add(boton2);
-      
-        JPanel panel = new JPanel();
-        JButton button = new JButton("Ir");
-        panel.add(button);
-        panel.setVisible(true);
-           
-         JFrame frame = new JFrame();
-         frame.setSize(200, 200);
-         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         frame.getContentPane().setLayout(null);
-      
-       ArrayList  suma = new ArrayList ();
+        //color del jpanel
+        this.getContentPane().setBackground(Color.CYAN);
+        jRadioButton1 = new JRadioButton();
+        jRadioButton2 = new JRadioButton();
+        jRadioButton1.setText("M");
+        jRadioButton2.setText("F");
+        jRadioButton1.setBackground(Color.GRAY);
+        jRadioButton2.setBackground(Color.GRAY);
 
-          final int NumeroMaximo = 5;
-          int numero=0;
-          
-          
-          while(numero != 5){
-            suma.add(numero);
-            numero++;
-            
-            System.out.print(suma.size());
-          }
+        jRadioButton1.setBounds(200, 30, 120, 50);
+        jRadioButton2.setBounds(315, 30, 120, 50);
+       
+        jButton = new JButton("SI");
+        jButton.setBounds(125, 120, 80, 30);
+        jButton2 = new JButton("NO");
+        jButton2.setBounds(225, 120, 80, 30);
+       
+        L1 = new JLabel("Elegir género");
+        L2 = new JLabel ("¿ Te ha gustado el programa?");
+        L1.setBorder(BorderFactory.createLineBorder(Color.black));
+        loweredbevel = BorderFactory.createLoweredBevelBorder();
+        L1.setBorder(loweredbevel);
+        L1.setBounds(20, 30, 150, 50);
+        L2.setBounds(110,60,250,100);
+       
+        this.add(jRadioButton1);
+        this.add(jRadioButton2);
+        this.add(jButton);
+        this.add(jButton2);
+        this.add(L1);
+        this.add(L2);
+       
+        G1 = new ButtonGroup();
+        G1.add(jRadioButton1);
+        G1.add(jRadioButton2);
+       
+       
+    }
+}
+
+class Boton {
+
+    public static void main(String args[]) {
+     
+    Fzo0424 f = new Fzo0424();
+       
+        f.setTitle("BOTÓN");
+       
+       
+   
+    }
+}
+
+
